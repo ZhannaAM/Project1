e3236a744fab4adaeb1de58841a768e3aa122589
ZhannaAM <zhmagera79@gmail.com>
2024-10-11 09:10:53 +0300

    mascs.py final

--- src/masks.py
+++ src/masks.py
@@ -5,9 +5,9 @@ account_str = int()
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает номер карты и возвращает ее маску 7000 79** **** 6361"""
-    return f"{card_number[:4]} {card_number[4:6]}** ****{card_number[-4:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account: str) -> str:
     """Функция принимает номер счета и возвращает его маску **4305 """
-    return f"{account[-4:]}"
+    return f"**{account[-4:]}"
